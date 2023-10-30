e6f7765db309bafff39ccbc09982e202f745853d
Samane-Gh <samanee.ghasemi>
2023-10-30 19:25:06 +0330

    Update best_time_to_buy_and_sell_stock.py

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -1,10 +1,15 @@
-import numpy as np
-prieces = np.array([])
-print(' please Enter length of array :')
-len = int(input())
-for i in range(0,len):
-    number = int(input())
-    prieces.append(number)
-print(prieces)
+def fun(prices, n):
+    buy = prices[0]
+    profit = 0
+    for i in range(1,n):
+        if(buy > prices[i]):
+            buy = prices[i]
+        elif(prices[i] - buy > profit ):
+            profit = prices[i] - buy
+    return profit
 
-max = prieces.max()
+print("Please enter number of prices")
+num_prices = int(input())
+print("Please enter items of price")
+prices = [int(input()) for i in range(num_prices)]
+print(fun(prices, num_prices))
